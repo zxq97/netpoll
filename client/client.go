1489da2b16078a359a272373c5cec543c4a70662
zongxingquan <zongxingquan@126.com>
2023-03-09 13:50:03 +0800

    epoll

--- client/client.go
+++ client/client.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
 	conn, err := net.Dial("tcp", ":8000")
@@ -10,6 +13,11 @@ func main() {
 	if _, err = conn.Write([]byte("123456")); err != nil {
 		panic(err)
 	}
+	buf := make([]byte, 1024)
+	if _, err = conn.Read(buf); err != nil {
+		panic(err)
+	}
+	fmt.Println(string(buf))
 	if err = conn.Close(); err != nil {
 		panic(err)
 	}
